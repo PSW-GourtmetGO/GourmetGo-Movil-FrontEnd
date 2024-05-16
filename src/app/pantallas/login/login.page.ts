66a9cb532d648ff071035868882873733eabc0af
ErikGranda3756 <75343631+ErikGranda3756@users.noreply.github.com>
2024-05-15 22:00:27 -0500

    Implementacion bienvenida y login

---
+++ src/app/pantallas/login/login.page.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage {
+  constructor(private router: Router) {}
+  olvidoClave() {
+    this.router.navigate(['/olvido-clave']);
+  }
+}
