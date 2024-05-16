55d20ef70f159d8c23e8034c680378d1880d14ef
Anthony Solis <asolis7378@uta.edu.ec>
2024-05-15 22:19:55 -0500

    Pantalla Olvido Contraseña

--- src/app/pantallas/olvido-clave/olvido-clave.page.ts
+++ src/app/pantallas/olvido-clave/olvido-clave.page.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-olvido-clave',
   templateUrl: './olvido-clave.page.html',
   styleUrls: ['./olvido-clave.page.scss'],
 })
-export class OlvidoClavePage implements OnInit {
+export class OlvidoClavePage {
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private router: Router) { }
+  registro() {
+    this.router.navigate(['/registro']);
   }
+
+
 
 }
