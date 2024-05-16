56ba196e1c95ba1140ab1b676a9ef2c7d4080af8
ChristianCLop <christian.chico@hotmail.com>
2024-05-16 09:27:32 -0500

    Creación Pagina Registro

--- src/app/pantallas/registro/registro.page.ts
+++ src/app/pantallas/registro/registro.page.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.page.html',
   styleUrls: ['./registro.page.scss'],
 })
-export class RegistroPage implements OnInit {
+export class RegistroPage {
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private router: Router) { }
+  login() {
+    this.router.navigate(['/login']);
   }
 
 }
